2611da91ff5aab31bc09a611f83b5cd4b3d759fa
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2019-03-07 12:44:34 -0600

    first commit

---
+++ Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 012
+Last Modified_______: 2019-03-07
+OU ID_______________: 113411201
+OU EMAIL____________: nathan.b.preuss-1@ou.edu
+
+Tasks:
+1. implement StackHospital class
+2. implement QueueHospital class
+3. implement PriorityQueue class
+4. implement sickPerson class
+5. implement healthyPerson class
+6. implement JUnit tests
+
+Estimated Time (minutes):
+1. 20
+2. 20
+3. 15
+4. 10
+5. 10
+6. 90
+
+Actual Time (minutes):
+1. 999
+2. 999
+3. 999
+4. 999
+5. 999
+6. 999
+*/}
